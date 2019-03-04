8f3f49eb8b974eacebabd67fd8d10c25f58c29b4
fr3d100 <f.bonnand@gmail.com>
2019-03-04 17:49:25 +0100

    add navbar

--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -1,4 +1,8 @@
 class UiController < ApplicationController
   def index
   end
+
+  def demo
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'ui/index'
+  get 'ui/demo'
+  get 'ui/homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
