6658f9868775575a0d57dc39ee3ef495721e005c
cacdesign <pro.nicolasfernandez@gmail.com>
2019-03-04 18:13:22 +0100

    add footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_03_04_145939) do
-
+ActiveRecord::Schema.define(version: 2019_03_04_145720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "properties", force: :cascade do |t|
     t.string "title"
