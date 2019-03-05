ea00aeece08bb8f59a380b0eaf06e8f7b179cd7c
Segrelove <mlesegret@gmail.com>
2019-03-05 15:23:54 +0100

    added a references to cart, tenant_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_111045) do
+ActiveRecord::Schema.define(version: 2019_03_05_142202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_03_05_111045) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "tenant_id"
+    t.index ["tenant_id"], name: "index_carts_on_tenant_id"
   end
 
   create_table "images", force: :cascade do |t|
