f492b9efa128ec80179f3d6181173cefc7d96c08
Segrelove <mlesegret@gmail.com>
2019-03-05 11:20:25 +0100

    added image table, removed column image_url from property, salut mathieu

---
+++ app/models/image.rb
@@ -0,0 +1,6 @@
+class Image < ApplicationRecord
+  belongs_to :property
+
+  validates :url, presence: true
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_145939) do
+ActiveRecord::Schema.define(version: 2019_03_05_100644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.bigint "property_id"
+    t.text "description"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["property_id"], name: "index_images_on_property_id"
+  end
 
   create_table "properties", force: :cascade do |t|
     t.string "title"
     t.decimal "price"
     t.integer "surface"
     t.string "description"
-    t.string "image_url"
     t.bigint "agent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
