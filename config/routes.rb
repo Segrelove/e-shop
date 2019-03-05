90e65ee3903cc4eed6d7ca10246dfb267667ffd5
cacdesign <pro.nicolasfernandez@gmail.com>
2019-03-05 11:38:27 +0100

    add show and steps views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'ui/index'
   get 'ui/demo'
   get 'ui/homepage'
+  get 'ui/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
