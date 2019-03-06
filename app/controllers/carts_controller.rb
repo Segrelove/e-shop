9bba0568901a96860c3480e13234dc0d7938732c
cacdesign <pro.nicolasfernandez@gmail.com>
2019-03-06 14:54:14 +0100

    add publishakle keys

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,22 @@
 class CartsController < ApplicationController
+
   def show
   	@cart = Cart.find(params[:id])
   	@reservations = @cart.reservations
+  	@total_price = total_price(@reservations)
   end
+
+
+
+private
+
+	def total_price(reservations)
+		total_price = 0
+		reservations.each do |reservation|
+			total_price += reservation.property.price
+			puts total_price
+		end
+		return total_price
+	end
+
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,5 @@ class Cart < ApplicationRecord
   has_many :reservations
   has_many :properties, through: :reservations
   belongs_to :user
+
 end
