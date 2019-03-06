ae4e09e63a366d6248fd7b91dab8a9121190dc1e
cacdesign <pro.nicolasfernandez@gmail.com>
2019-03-06 10:48:51 +0100

    model acheived

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,6 @@ class Order < ApplicationRecord
   has_many :join_table_order_properties
   has_many :properties, through: :join_table_order_properties
 
-  belongs_to :tenant
+  belongs_to :tenant, class_name: "User"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_173452) do
+ActiveRecord::Schema.define(version: 2019_03_06_092100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "carts", force: :cascade do |t|
-    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.boolean "current"
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
