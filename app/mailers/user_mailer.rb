5eec7adfc250a64d64ad40f89269daa684bd9b54
fr3d100 <f.bonnand@gmail.com>
2019-03-07 20:02:50 +0100

    Fix email wrong address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @url  = 'http://ding-dong-production-herokuapp.com/login'
+    @url  = 'http://ding-dong-production.herokuapp.com/login'
     mail(to: @user.email, subject: 'Bienvenue chez Ding Dong !')
   end
 end
